8ae45f2c7e1708adeb628fa511a14a21ac1070c3
Lydia Ho <lydiaho@wustl.edu>
2019-04-07 18:24:58 -0500

    Update sunburst.js

--- docs/js/sunburst.js
+++ docs/js/sunburst.js
@@ -66,7 +66,7 @@ SunburstDisplay.prototype.update = function() {
 
     var g = vis.svg.append("g");
     var colorInner = d3.scaleOrdinal(d3.schemeCategory10);
-    var colorMiddle = d3.scaleOrdinal(d3.schemeCategory20);
+    var colorMiddle = d3.scaleOrdinal(d3.schemeSet3);
     var colorOuter = d3.scaleOrdinal(d3.schemeCategory30);
 
     g.selectAll('path')
