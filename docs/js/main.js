077c544ec95d8da53add5eb82d1c395b89b8871a
Emily Wilson <emwilson@wustl.edu>
2019-04-06 19:04:45 -0500

    main and sunburst changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,72 +6,72 @@ var sunburst, cluster, senten;
 loadData();
 
 function loadData() {
-    d3.json("data/profiles_wrangled.json", function(data) {
-        console.log(data);
-        allData = data;
-        createVis();
-    })
-    // d3.csv("data/profiles_wrangled.csv", function(error, data) {
-    //     if (error) {
-    //         console.log("Error loading data: " + error);
-    //     } else {
-    //         var i = 0;
-    //         data.forEach(function(d) {
-    //             delete d[""];
-    //             delete d["Unnamed: 0"];
+    // d3.json("data/profiles_wrangled.json", function(data) {
+    //     console.log(data);
+    //     allData = data;
+    //     createVis();
+    // })
+    d3.csv("data/profiles_wrangled.csv", function(error, data) {
+        if (error) {
+            console.log("Error loading data: " + error);
+        } else {
+            var i = 0;
+            data.forEach(function(d) {
+                delete d[""];
+                delete d["Unnamed: 0"];
 
-    //             d.age = parseInt(d.age);
-    //             d.asian = d.asian == "TRUE";
-    //             d.black = d.black == "TRUE";
-    //             d.endagebucket10 = parseInt(d.endagebucket10);
-    //             d.haspets = d.haspets == "TRUE";
-    //             d.height = parseInt(d.height);
-    //             d.hispanic = d.hispanic == "TRUE";
-    //             d.indian = d.indian == "TRUE";
-    //             d.middle_eastern = d.middle_eastern == "TRUE";
-    //             d.native_american = d.native_american == "TRUE";
-    //             d.other = d.other == "TRUE";
-    //             d.pacific_islander = d.pacific_islander == "TRUE";
-    //             d.startagebucket10 = parseInt(d.startagebucket10);
-    //             d.white = d.white == "TRUE";
-    //             d.ethnicityArr = parseEthnicities(d);
-    //             d.ethnicityGroup = ethnicityGrouper(d);
-    //             // var loc = d.location.split(", ");
-    //             // d.city = loc[0];
-    //             // d.state = loc[1];
-    //             // if (loc.length == 3) {
-    //             //     d.country = loc[2];
-    //             // } else {
-    //             //     d.country = "usa";
-    //             // }
-    //             // console.log(d.city + " " + d.state + " " + d.country);
+                d.age = parseInt(d.age);
+                d.asian = d.asian == "TRUE";
+                d.black = d.black == "TRUE";
+                d.endagebucket10 = parseInt(d.endagebucket10);
+                d.haspets = d.haspets == "TRUE";
+                d.height = parseInt(d.height);
+                d.hispanic = d.hispanic == "TRUE";
+                d.indian = d.indian == "TRUE";
+                d.middle_eastern = d.middle_eastern == "TRUE";
+                d.native_american = d.native_american == "TRUE";
+                d.other = d.other == "TRUE";
+                d.pacific_islander = d.pacific_islander == "TRUE";
+                d.startagebucket10 = parseInt(d.startagebucket10);
+                d.white = d.white == "TRUE";
+                d.ethnicityArr = parseEthnicities(d);
+                d.ethnicityGroup = ethnicityGrouper(d);
+                // var loc = d.location.split(", ");
+                // d.city = loc[0];
+                // d.state = loc[1];
+                // if (loc.length == 3) {
+                //     d.country = loc[2];
+                // } else {
+                //     d.country = "usa";
+                // }
+                // console.log(d.city + " " + d.state + " " + d.country);
 
                 
-    //             // if (i < 1000) {
-    //             // for (var key in d) {
+                // if (i < 1000) {
+                // for (var key in d) {
                     
-    //             //     //console.log(d[key]);
-    //             //     if (dataCats[key] == null) {
-    //             //         dataCats[key] = [];
-    //             //     }
-    //             //     if (!dataCats[key].includes(d[key])) {
-    //             //         dataCats[key].push(d[key]);
-    //             //     }
+                //     //console.log(d[key]);
+                //     if (dataCats[key] == null) {
+                //         dataCats[key] = [];
+                //     }
+                //     if (!dataCats[key].includes(d[key])) {
+                //         dataCats[key].push(d[key]);
+                //     }
 
-    //             // }
-    //             //    i = i + 1;
-    //             // }
+                // }
+                //    i = i + 1;
+                // }
                 
                 
 
-    //         });
-    //         console.log(data);
-    //         allData = data;
+            });
+            console.log(data);
+            allData = data;
 
-    //         createVis();
+            createVis();
 
-    //     }
-    // });
+        }
+    });
 }
 
 function parseEthnicities(d) {
